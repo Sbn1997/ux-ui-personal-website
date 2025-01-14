70965efbcf92bddc55a1ce60fc538a65dcd2da60
ShaharBenNaim <shaharben1997@gmail.com>
2025-01-14 22:17:11 +0200

    profile-section css width and height

--- src/components/ProfileSection.js
+++ src/components/ProfileSection.js
@@ -4,12 +4,6 @@ function ProfileSection() {
   return (
     <div className='profile-wrapper'>
       <div className="profile-section">
-        <h1>Or Nadav</h1>
-        <h2>UX/UI Designer</h2>
-        <p>
-          Hi, I'm Or Nadav, a dedicated UX/UI designer passionate about creating intuitive and visually appealing digital experiences.
-        </p>
-        <button>About Me</button>
       </div>
     </div>
   );
