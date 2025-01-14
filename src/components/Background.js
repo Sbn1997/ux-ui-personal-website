8929df55a5a749adee01db896e548e14075ced8e
ShaharBenNaim <shaharben1997@gmail.com>
2025-01-14 21:45:55 +0200

    background children

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import '../App.css'; 
 
-function Background() {
+function Background({children}) {
   return (
   <div className='background-wrapper'>
     <div className="background">
         <div className="shape top-shape"></div>
         <div className="shape mid-shape"></div>
         <div className="shape down-shape"></div>
+        <div>{children}</div>
     </div>
 </div>
   );
